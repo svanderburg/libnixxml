284723383c4e946259097791834466c47dd5232e
Sander van der Burg <svanderburg@gmail.com>
2019-05-11 15:40:28 +0200

    Fix running tests in a chroot environment

---
+++ tests/generic/setup.sh
@@ -0,0 +1,2 @@
+export TEST_ROOT=$(realpath ${TMPDIR:-/tmp}/nix-test)
+export NIX_STATE_DIR=$TEST_ROOT/var/nix
--- tests/pp-glib/test-identical.sh
+++ tests/pp-glib/test-identical.sh
@@ -1,4 +1,6 @@
-#!/bin/sh -e
+set -e
+
+source setup.sh
 
 ../../src/nixxml-pp/nixxml-pp -f nix test-ast.xml > test-ast2.nix
 sh nixexpr2xml --attr-style verbose --enable-types test-ast2.nix > test-ast2.xml
